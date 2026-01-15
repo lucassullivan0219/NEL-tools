909ada64397a79fae9c2fc8ab62eb961a32f1db8
lucassullivan0219 <lucassullivan0219@gmail.com>
2026-01-15 02:21:33 +0000

    新增測試檔案

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 
+# Updated setup.py
 from setuptools import setup, find_packages
 
 setup(
-    name="nel_tools",                # 套件名稱 (pip install 時顯示的名字)
-    version="0.1.0",                 # 版本號
-    packages=find_packages(),        # 自動偵測你的 mat_tools 資料夾
-    install_requires=[               # 自動安裝依賴套件 (Robustness)
+    name="mat_tools",                # Change this to match your folder name
+    version="0.1.0",
+    packages=find_packages(),        # This will find the 'mat_tools' directory
+    install_requires=[
         "numpy",
         "scipy",
         "h5py"
     ],
-    author="Lucas Sullivan",         # 你的名字 (可選)
-    description="A simple tool for loading .mat files with v7.3 support",
     python_requires='>=3.6',
 )
